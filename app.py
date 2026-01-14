9c6938cb5b396654e90fded3de8df2a51113d54a
Lucas DAWEB <lvazgar131@fp.iesromerovargas.com>
2026-01-14 14:05:19 +0100

    subida de app

---
+++ app.py
@@ -0,0 +1,14 @@
+# app.py
+"""docstring del modulo"""
+def suma(a:int,b:int) -> int:
+    """docstring funcion
+
+    Args:
+        a (int): numero a para sumar
+        b (int): numero a para sumar
+
+    Returns:
+        int: suma de ambos
+    """
+    x = "no se usa"
+    return a+ b
